a5c07d39ee0f5ed1729c8ac74b1604f8daa0a4af
Александр <aleksrowing232@gmail.com>
2022-03-01 01:11:58 +0300

    cssphone_desktop

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import './header.css'
 
@@ -10,8 +11,14 @@ const Header=()=>{
         <div class="container-12 relative" id='module_phone'>
             <div id="utp1" class="content-editable utp" >Промышленный альпинизм и высотные работы</div>
            <a href="https://wa.me/+79063207897"><div class="btn">Напиши</div></a> 
+           <div className="grid_header">
+               <Link className="link_header" to="/about">О нас</Link>
+               <Link  className='link_header' to="/jobs">Виды работ</Link>
+                <Link className='link_header' to="/schema">Схема работ</Link>
+                <Link className='link_header' to="/photo">Фотогалерея</Link>
+           </div>
         </div>
-        <a class="phone-button" href="tel:+79063207897">&#9742;</a>
+        <a class="phone-button" href="tel:+79063207897"><img className="img_phone" src="https://img.icons8.com/color/48/000000/phone-message--v2.gif" /></a>
     </div>
 
 
